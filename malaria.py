17528b295697157eb2b0576f158bce0b068875d0
Aaris-Kazi <aariskazi@gmail.com>
2022-02-20 20:23:44 +0530

    cleaning code

--- malaria.py
+++ malaria.py
@@ -4,17 +4,16 @@ from keras.applications.mobilenet import preprocess_input
 from keras.preprocessing import image
 from keras.models import load_model
 
-model = load_model("models/xray_model_final.h5")
+model = load_model("models/malaria_cnn.h5")
 
-
-def pneumo(img):
-    img = image.load_img(img, target_size=(224, 224))
+def mala(img):
+    img = image.load_img(img, target_size=(50, 50))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     output = preprocess_input(x)
     output = model(output)
     output = np.array(output)
-    if np.argmax(output) == 1:
+    if np.argmax(output) == 0:
         v = 'The sample is positive'
     else:
         v = 'The sample is negative'
