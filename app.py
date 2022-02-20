917e31b195d4018444f1cc2305623e79d114eb43
Aaris-Kazi <aariskazi@gmail.com>
2022-02-20 20:58:21 +0530

    minute changes

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from tomlkit import value
 from werkzeug.utils import secure_filename
 import os
 from cancer_model import can
