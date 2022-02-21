80be9420458fb8f22a7d5b53c987ec2250b21ba6
Aaris-Kazi <aariskazi@gmail.com>
2022-02-21 20:57:54 +0530

    pneumonia changes

--- pneumonia_model.py
+++ pneumonia_model.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import array, expand_dims, argmax
 from keras.applications.mobilenet import preprocess_input
 # import matplotlib.pyplot as plt
 from keras.preprocessing import image
@@ -10,11 +10,11 @@ model = load_model("models/xray_model_final.h5")
 def pneumo(img):
     img = image.load_img(img, target_size=(224, 224))
     x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
+    x = expand_dims(x, axis=0)
     output = preprocess_input(x)
     output = model(output)
-    output = np.array(output)
-    if np.argmax(output) == 1:
+    output = array(output)
+    if argmax(output) == 1:
         v = 'The sample is positive'
     else:
         v = 'The sample is negative'
