b68129f349d6744967d0acfd1ac31096eebbb548
Aaris-Kazi <aariskazi@gmail.com>
2022-02-26 11:59:19 +0530

    app changes

--- app.py
+++ app.py
@@ -149,5 +149,5 @@ def get_my_ip():
     return jsonify({'ip': request.remote_addr}), 200
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', debug=True)
