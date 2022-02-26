5d902b2791352c3911c1b1d72fb33015c29b4f93
Aaris-Kazi <aariskazi@gmail.com>
2022-02-26 11:59:51 +0530

    cacer model 🦀

--- cancer_model.py
+++ cancer_model.py
@@ -1,10 +1,11 @@
 from joblib import load
-from cv2 import imread
+from keras.preprocessing import image
 from numpy import array
 
 def can(img):
     rfc = load('models/multispec_rfc15.pkl')
-    img = imread(img)
+    # img = imread(img)
+    img = image.load_img(img)
     pimg = array(img).flatten()
     p = rfc.predict([pimg])
     if p[0] == '1':
