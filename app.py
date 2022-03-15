abea14a4e49fb3251ae060cf17eeca0d16d221a3
Aaris-Kazi <aariskazi@gmail.com>
2022-03-15 16:35:06 +0530

    app changes

--- app.py
+++ app.py
@@ -154,6 +154,8 @@ def test_images():
 @app.route("/get_my_ip", methods=["GET"])
 def get_my_ip():
     return jsonify({'ip': request.remote_addr}), 200
+    # return request.environ.get('HTTP_X_REAL_IP', request.remote_addr) 
+    # return request.remote_addr
 
 if __name__ == '__main__':
     app.run(debug=True)
